67d044044c95a5eef105d50ec7e435f7bc3b3466
Abdul Rahiman Jasbeer K A <abdulrahmanjasbeer8486@gmail.com>
2025-05-10 10:44:19 +0530

    user reg

--- chatbot/models.py
+++ chatbot/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    phone = models.CharField(max_length=10, blank=True, null=True)
+
+    def __str__(self):
+        return self.username
 
 
 class ChatHistory(models.Model):
